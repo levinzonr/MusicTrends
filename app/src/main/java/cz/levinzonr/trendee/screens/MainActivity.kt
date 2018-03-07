e68a3977ed47ca20b52066d2c938f178782a628e
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-07 22:13:12 +0100

    Package structure

--- app/src/main/java/cz/levinzonr/trendee/screens/MainActivity.kt
+++ app/src/main/java/cz/levinzonr/trendee/screens/MainActivity.kt
@@ -1,10 +1,11 @@
-package cz.levinzonr.trendee
+package cz.levinzonr.trendee.screens
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import cz.levinzonr.trendee.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
