546225b3444a5d24c2d759c0d80080121cf8ee9d
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-08 15:03:54 +0100

    View callbacks implemented

--- app/src/main/java/cz/levinzonr/trendee/presenter/ArtistListPresenter.kt
+++ app/src/main/java/cz/levinzonr/trendee/presenter/ArtistListPresenter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import cz.levinzonr.trendee.api.LastFmClient
 import cz.levinzonr.trendee.model.Artist
 import cz.levinzonr.trendee.model.ArtistResponse
+import cz.levinzonr.trendee.screens.ViewCallbacks
 import cz.levinzonr.trendee.screens.artistslist.ArtistsListFragment
 import rx.Subscriber
 import rx.Subscription
@@ -13,18 +14,18 @@ import rx.schedulers.Schedulers
 /**
  * Created by nomers on 3/8/18.
  */
-class ArtistListPresenter : Presenter<ArtistsListFragment>, Subscriber<ArtistResponse>(){
+class ArtistListPresenter : Presenter<ViewCallbacks<List<Artist>>>, Subscriber<ArtistResponse>(){
 
     companion object {
         const val TAG = "ArtistListPresenter"
     }
 
-    private var view: ArtistsListFragment? = null
+    private var view: ViewCallbacks<List<Artist>>?= null
     private var subscription: Subscription? = null
-    private var items: List<Artist>? = null
+    private lateinit var items: List<Artist>
 
 
-    override fun attachView(view: ArtistsListFragment) {
+    override fun attachView(view: ViewCallbacks<List<Artist>>) {
         this.view = view
     }
 
@@ -36,6 +37,7 @@ class ArtistListPresenter : Presenter<ArtistsListFragment>, Subscriber<ArtistRes
         if (subscription != null){
             subscription?.unsubscribe()
         }
+        view?.onLoadingStart()
         subscription = LastFmClient.instance().getTrendingArtists()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
@@ -46,8 +48,7 @@ class ArtistListPresenter : Presenter<ArtistsListFragment>, Subscriber<ArtistRes
 
     override fun onNext(t: ArtistResponse?) {
         if (t != null) {
-            Log.d(TAG, t.artists.toString())
-
+            this.items = t.artists.artist
         } else {
             Log.d(TAG, "responce null")
 
@@ -56,9 +57,11 @@ class ArtistListPresenter : Presenter<ArtistsListFragment>, Subscriber<ArtistRes
 
     override fun onCompleted() {
         Log.d(TAG, "Items Loaded ${this.items}")
+        view?.onLoadingFinished(this.items)
     }
 
     override fun onError(e: Throwable?) {
+        view?.onError()
         Log.d(TAG, e.toString())
     }
 }
--- app/src/main/java/cz/levinzonr/trendee/screens/artistslist/ArtistsListFragment.kt
+++ app/src/main/java/cz/levinzonr/trendee/screens/artistslist/ArtistsListFragment.kt
@@ -3,18 +3,26 @@ package cz.levinzonr.trendee.screens.artistslist
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import cz.levinzonr.trendee.R
+import cz.levinzonr.trendee.model.Artist
 import cz.levinzonr.trendee.presenter.ArtistListPresenter
+import cz.levinzonr.trendee.screens.ViewCallbacks
+import java.time.LocalDate
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class ArtistsListFragment : Fragment() {
+class ArtistsListFragment : Fragment(), ViewCallbacks<List<Artist>>{
+
+    companion object {
+        const val TAG = "ArtistListFragment"
+    }
 
     private lateinit var presenter: ArtistListPresenter
 
@@ -25,6 +33,18 @@ class ArtistsListFragment : Fragment() {
         presenter.attachView(this)
         presenter.fetchTrendingPage()
         return inflater!!.inflate(R.layout.fragment_artists_list, container, false)
+    }
+
+    override fun onLoadingStart() {
+        Log.d(TAG, "Loading start")
+    }
+
+    override fun onLoadingFinished(result: List<Artist>) {
+        Log.d(TAG, "Loading finished: $result")
+    }
+
+    override fun onError() {
+        Log.d(TAG, "Error")
     }
 
     override fun onDestroy() {
