64b3abc92b2c255c36afd24c68f02d695d00e178
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-08 11:49:31 +0100

    LastFm api set-up

---
+++ app/src/main/java/cz/levinzonr/trendee/api/LastFmClient.kt
@@ -0,0 +1,45 @@
+package cz.levinzonr.trendee.api
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import cz.levinzonr.trendee.model.Artist
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.Observable
+
+/**
+ * Created by nomers on 3/8/18.
+ */
+class LastFmClient {
+
+    private val gson: Gson =
+            GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create()
+
+    private val retrofit : Retrofit  = Retrofit.Builder()
+            .baseUrl(ROOT_URL)
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    var service: LastFmService = retrofit.create(LastFmService::class.java)
+
+    companion object {
+        const val ROOT_URL = "http://ws.audioscrobbler.com/2.0/"
+        const val API_KEY = "0b3c38cc4001ab622a02719ef52b0563"
+
+        var instance: LastFmClient? = null
+            get() {
+                if (field == null) {
+                    field = LastFmClient()
+                }
+                return field
+            }
+    }
+
+    fun getTrendingArtists(): Observable<List<Artist>> {
+        return service.getTrendingArtists()
+    }
+
+}
---
+++ app/src/main/java/cz/levinzonr/trendee/api/LastFmService.kt
@@ -0,0 +1,14 @@
+package cz.levinzonr.trendee.api
+
+import cz.levinzonr.trendee.model.Artist
+import retrofit2.http.GET
+import rx.Observable
+
+/**
+ * Created by nomers on 3/8/18.
+ */
+interface LastFmService {
+
+    @GET("?method=chart.gettopartists&api_key=${LastFmClient.API_KEY}&format=json")
+    fun getTrendingArtists() : Observable<List<Artist>>
+}
